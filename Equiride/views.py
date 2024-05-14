11cda6de0ed7e0b23e27713a8b90b9194957d53b
rachael baikey <rachaelacb@gmail.com>
2024-05-14 15:33:23 +0000

    discard changes

--- Equiride/views.py
+++ Equiride/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#from django.shortcuts import render
+#from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def my_Equiride(request):
-    return HttpResponse("Hello, Booking System!")
+#def my_Equiride(request):
+#   return HttpResponse("Hello, Booking System!")
 
-def homepage(request):
-    return render(request, 'homepage.html')
+#def homepage(request):
+#   return render(request, 'homepage.html')
+
+class Index(TemplateView):
+    template_name = "Equiride/templates/home/index.html"
